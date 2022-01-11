87252ec584086788b150234ad9f30020d98e874c
edoardob90 <edoardob90@gmail.com>
2022-01-11 17:41:42 +0100

    Add 'get_records' method to Spreadsheet class

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,8 @@
-from typing import List, NoReturn, Any
+from typing import Dict, List, NoReturn, Any, Union
 
 from gspread import Client, Worksheet
-from gspread.exceptions import APIError
+from gspread import Spreadsheet as Document
+from gspread.exceptions import APIError, WorksheetNotFound
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,9 +19,9 @@ class Spreadsheet():
         # Properties
         self._spreadsheet_id: str = spreadsheet_id # the spreadsheet ID
         self._sheet_name: str = sheet_name # the sheet name
-        self._range: str = None # the table range
+        self._doc: Document = None
         self._sheet: Worksheet = None
-        self._records: List = None
+        self._range: str = None # the table range
 
     # Properties with accessors
     @property
@@ -41,12 +42,29 @@ class Spreadsheet():
         self._sheet_name = name
 
     @property
-    def sheet(self) -> Worksheet:
+    def doc(self) -> Document:
+        if not self.spreadsheet_id:
+            raise SpreadsheetError('Spreadsheet ID is undefined!')
+
         try:
             doc = self._client.open_by_key(self.spreadsheet_id)
-            sheet = doc.worksheet(self.sheet_name)
         except APIError:
-            raise SpreadsheetError("Spreadsheet ID {} was not found.".format(self.spreadsheet_id))
+            raise SpreadsheetError("A spreadsheet with ID '{}' was not found.".format(self.spreadsheet_id))
+
+        self._doc = doc
+
+        return self._doc
+    
+    @doc.setter
+    def doc(self, id_: str = None) -> NoReturn:
+        raise SpreadsheetError('Spreadsheet document should only be set by providing the spreadsheet ID.')
+
+    @property
+    def sheet(self) -> Worksheet:
+        try:
+            sheet = self.doc.worksheet(self.sheet_name)
+        except (APIError, WorksheetNotFound):
+            raise SpreadsheetError("A sheet with name '{}' does not exist!".format(self.sheet_name))
         
         self._sheet = sheet
         
@@ -68,3 +86,24 @@ class Spreadsheet():
     def append_records(self, values: List[List]) -> Any:
         """Append one or more records to the spreadhseet"""
         return self.sheet.append_rows(values, value_input_option="USER_ENTERED", table_range=self.range)
+
+    def get_records(self, sheet_name: str = None, range_: str = None, as_dict: bool = False, **kwargs) -> List | List[Dict]:
+        """Get records from a range (A1 notation) or an entire sheet"""
+        # If `sheet` is not given, check if it's already been set
+        if sheet_name:
+            self.sheet_name = sheet_name
+        
+        if self.sheet_name is None:
+            all_sheets_names = [str(sheet.title) for sheet in self.doc.worksheets()]
+            raise SpreadsheetError(f"Cannot get data from an unnamed sheet! Available sheets: '{', '.join(all_sheets_names)}'")
+        
+        # Set the range if given
+        if range_:
+            self.range = range_
+        
+        if self.range:
+            values = self.sheet.get_values(self.range)
+        else:
+            values = self.sheet.get_all_records(**kwargs) if as_dict else self.sheet.get_all_values()
+    
+        return values
