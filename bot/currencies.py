818edb0eec57301cf203ef20282701370dc5c5ee
edoardob90 <edoardob90@gmail.com>
2023-11-07 17:44:47 +0100

    Greatly improved currency parser

--- bot/currencies.py
+++ bot/currencies.py
@@ -1,28 +1,165 @@
+import re
 import typing as t
+from itertools import chain
 
 CURRENCIES: t.Dict[str, t.Dict] = {
     "EUR": {"symbol": "€", "name": "Euro", "aliases": ("E", "e", "€", "eur", "euro")},
-    "USD": {"symbol": "$", "name": "US Dollar", "aliases": ("U", "$", "usd", "dollar")},
+    "USD": {
+        "symbol": "$",
+        "name": "US Dollar",
+        "aliases": ("U", "u", "$", "usd", "dollar"),
+    },
     "CHF": {
         "symbol": "CHF",
         "name": "Swiss Franc",
-        "aliases": ("C", "c", "chf", "franc", "Sfr."),
+        "aliases": ("C", "c", "chf", "franc", "Sfr.", "sfr.", "Sfr", "sfr"),
     },
     "GBP": {
         "symbol": "£",
         "name": "British Pound",
-        "aliases": ("G", "£", "gbp", "pound"),
+        "aliases": ("G", "g", "£", "gbp", "pound"),
     },
 }
 
 CURRENCIES_CODES: t.List[str] = list(CURRENCIES.keys())
 CURRENCIES_SYMBOLS: t.List[str] = [c["symbol"] for c in CURRENCIES.values()]
+CURRENCIES_ALIASES: t.Set[str] = set(
+    chain.from_iterable(c["aliases"] for c in CURRENCIES.values())
+)
 
 
-def symbol_from_str(alias: str | None) -> str:
+def or_regex(symbols: t.List[str] | t.Set[str]) -> re.Pattern:
+    """Return a regex pattern that matches any of the given symbols"""
+    return re.compile(r"|".join(re.escape(s) for s in symbols))
+
+
+def extract_currency(amount: str) -> str | None:
+    """Extract the currency symbol from an amount string"""
+    if not amount:
+        return None
+
+    _search_methods = [
+        or_regex(CURRENCIES_CODES).search,
+        or_regex(CURRENCIES_SYMBOLS).search,
+        or_regex(CURRENCIES_ALIASES).search,
+    ]
+
+    for method in _search_methods:
+        if (match := method(amount)) is not None:
+            return match.group(0)
+
+    return None
+
+
+def extract_number(amount: str) -> str | None:
+    """
+    Extract the number part from an amount string
+    Adapted from https://github.com/scrapinghub/price-parser
+    """
+    amount = re.sub(
+        r"\s+", " ", amount
+    )  # clean initial text from non-breaking and extra spaces
+
+    if amount.count("€") == 1:
+        m = re.search(
+            r"""
+        [\d\s.,]*?\d    # number, probably with thousand separators
+        \s*?€(\s*?)?    # euro, probably separated by whitespace
+        \d(?(1)\d|\d*?) # if separated by whitespace - search one digit, multiple digits otherwise
+        (?:$|[^\d])     # something which is not a digit
+        """,
+            amount,
+            re.VERBOSE,
+        )
+
+        if m:
+            return m.group(0).replace(" ", "")
+
+    m = re.search(
+        r"""
+        ([-]?[.]?\d[\d\s.,']*)   # number, probably with thousand separators
+        \s*?                    # skip whitespace
+        (?:[^%\d]|$)            # capture next symbol - it shouldn't be %
+        """,
+        amount,
+        re.VERBOSE,
+    )
+
+    if m:
+        price_text = m.group(1).rstrip(",.")
+        return (
+            price_text.strip()
+            if price_text.count(".") == 1
+            else price_text.lstrip(",.").strip()
+        )
+
+    if "free" in amount.lower():
+        return "0"
+
+    return None
+
+
+def parse_number(number: str | None) -> float | None:
+    if not number:
+        return None
+
+    number = number.strip().replace(" ", "").replace("'", "")
+
+    _search_decimal_sep = re.compile(
+        r"""
+            \d*        # null or more digits (there can be more before it)
+            ([.,€])    # decimal separator
+            (?:        # 1,2 or 4+ digits. 3 digits is likely to be a thousand separator
+            \d{1,2}?|
+            \d{4}\d*?
+            )
+            $
+        """,
+        re.VERBOSE,
+    )
+
+    decimal_separator = None
+
+    if match := _search_decimal_sep.search(number):
+        decimal_separator = match.group(1)
+
+    if decimal_separator is None:
+        number = number.replace(".", "").replace(",", "")
+    elif decimal_separator == ".":
+        number = number.replace(",", "")
+    elif decimal_separator == ",":
+        number = number.replace(".", "").replace(",", ".")
+    else:
+        assert decimal_separator == "€"
+        number = number.replace(".", "").replace(",", "").replace("€", ".")
+
+    try:
+        return float(number)
+    except ValueError:
+        return None
+
+
+def currency_parser(number_str: str | t.Any) -> t.Tuple[float, str | None]:
+    """Parse a number string that contains a currency symbol or name"""
+    _number_str = number_str if isinstance(number_str, str) else str(number_str)
+
+    # Extract the currency part
+    currency = extract_currency(_number_str)
+
+    # Extract the number part
+    amount = extract_number(_number_str)
+
+    # Parse the number
+    number = parse_number(amount)
+
+    return round(number, 2) if number else 0.0, symbol_from_str(currency)
+
+
+def symbol_from_str(alias: str | None) -> str | None:
     """Return the symbol of a currency from its alias"""
-    for currency, props in CURRENCIES.items():
-        if alias in props["aliases"]:
-            return currency
+    if not alias:
+        return
 
-    return ""
+    for currency, props in CURRENCIES.items():
+        if alias == currency or alias in props["aliases"]:
+            return currency
---
+++ tests/test_currencies.py
@@ -0,0 +1,34 @@
+import pytest
+
+from bot.currencies import currency_parser
+
+
+def test_amount_string():
+    amount = "1,000.00"
+    assert currency_parser(amount) == (1000.00, None)
+
+
+def test_amount_float():
+    amount = -1000.00
+    assert currency_parser(amount) == (-1000.00, None)
+
+
+def test_empty_amount():
+    amount = ""
+    assert currency_parser(amount) == (0.00, None)
+
+
+@pytest.mark.parametrize(
+    "amount, expected",
+    [
+        ("-10e", (-10.00, "EUR")),
+        ("99$", (99.00, "USD")),
+        ("9usd", (9.0, "USD")),
+        ("-666 c", (-666.00, "CHF")),
+        ("1000 CHF", (1000.0, "CHF")),
+        ("-1,000.01 €", (-1000.01, "EUR")),
+        ("£1'009", (1009.0, "GBP")),
+    ],
+)
+def test_amounts(amount, expected):
+    assert currency_parser(amount) == expected
